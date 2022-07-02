ca4565dfb1617a2b2088568f722c314525a08e72
luiz <luiz.qos@gmail.com>
2022-07-02 15:35:26 -0300

    Adicionando conetor

---
+++ conexao.php
@@ -0,0 +1,10 @@
+<?php
+	$servidor = "";
+	$usuario = "";
+	$senha = "";
+	$dbname = "";
+	
+	$access_token = "";
+	//Criar a conexão
+	$conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
+?>
