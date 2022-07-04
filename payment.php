2e7edd3d6fe94df8d664d74c54bacd1482b0e728
Luiz Antonio Alves da Silva <luiz.silva@grupozelo.com>
2022-07-04 00:33:06 -0300

    Integrando com pix

--- payment.php
+++ payment.php
@@ -1,5 +1,18 @@
 <?php
     include_once 'conexao.php';
+
+    
+function string_between_two_string($str, $starting_word, $ending_word)
+{
+    $subtring_start = strpos($str, $starting_word);
+    //Adding the strating index of the strating word to 
+    //its length would give its ending index
+    $subtring_start += strlen($starting_word);  
+    //Length of our required sub string
+    $size = strpos($str, $ending_word, $subtring_start) - $subtring_start;  
+    // Return the substring from the index substring_start of length size 
+    return substr($str, $subtring_start, $size);  
+}
   
   if(isset($_POST)){
 
@@ -7,7 +20,56 @@
 
       if($_POST['pix']){
 
-        $valor = 10;
+        $query = "select  pp.id 
+        , pp.idVenda
+        , pp.codePix
+        , pp.qrCode
+        , pp.dataCreated
+        , l.valor
+        , l.clientes_id
+        , l.descricao
+        , l.usuarios_id
+        , c.nomeCliente
+        , c.email
+        , c.cep
+        , c.rua
+        , c.numero
+        , c.bairro
+        , c.cidade
+        , c.estado
+        , DATE_ADD(pp.dataCreated, INTERVAL 1 DAY) as validadePix
+        from pagamentos_pix as pp
+        inner join lancamentos as L
+        on pp.idVenda = l.vendas_id
+        inner join clientes as c
+        on l.clientes_id = c.idClientes
+        where l.vendas_id = 30
+        and l.baixado = 0
+        order by pp.id desc
+        LIMIT 1";
+
+        $result = mysqli_query($conexao,$query);
+
+        foreach($result as $r) {
+            $id = $r['id'];
+            $idVenda = $r['idVenda'];
+            $codePix = $r['codePix'];
+            $qrCode = $r['qrCode'];
+            $dataCreated = $r['dataCreated'];
+            $valor = $r['valor'];
+            $nomeCliente = $r['nomeCliente'];
+            $email = $r['email'];
+            $cep = str_replace('-', '', $r['cep']);
+            $rua = $r['rua'];
+            $numero = $r['numero'];
+            $bairro = $r['bairro'];
+            $cidade = $r['cidade'];
+            $uf = $r['estado'];
+            $validadePix = $r['validadePix'];
+            $ref = string_between_two_string($r['descricao'], 'Ref: ', ' N');
+            $descricao = 'Lista Iptv | Ref:'.$ref.' | Venda: '. $idVenda;
+        }
+        mysqli_close($conexao);
 
         require_once 'mercadopago/lib/mercadopago/vendor/autoload.php';
 
@@ -15,29 +77,40 @@
 
 
   			$payment = new MercadoPago\Payment();
-  			$payment->description = 'Pagamento Nome';
+  			$payment->description = $descricao;
   			$payment->transaction_amount = (double)$valor;
   			$payment->payment_method_id = "pix";
 
-  			$payment->notification_url   = 'https://seusite.com/notification.php';
-  			$payment->external_reference = '1520';
+  			$payment->notification_url  = 'https://seusite.com/notification.php';
+  			$payment->external_reference = $idVenda;
 
   				$payment->payer = array(
-  					"email" => 'emailcliente@gmail.com',
-  					"first_name" => 'Primeiro nome do cliente',
+  					"email" => $email,
+  					"first_name" => $nomeCliente,
   					"address"=>  array(
-  						"zip_code" => "06233200",
-  						"street_name" => "Av. das Nações Unidas",
-  						"street_number" => "3003",
-  						"neighborhood" => "Bonfim",
-  						"city" => "Osasco",
-  						"federal_unit" => "SP"
+  						"zip_code" => $cep,
+  						"street_name" => $rua,
+  						"street_number" => $numero,
+  						"neighborhood" => $bairro,
+  						"city" => $cidade,
+  						"federal_unit" => $uf
   					)
   				);
 
   				$payment->save();
 
-         echo json_encode($payment->point_of_interaction);
+        //echo json_encode($payment->point_of_interaction);
+        
+
+        echo json_encode(
+        array(
+          'pix'  => $payment->point_of_interaction,
+          'dados'=>  array(
+          'idVenda'  => $idVenda,
+          'valor' => $valor,
+          'ref' => $ref
+          )
+        ));
 
       }else{
         echo json_encode(array(
