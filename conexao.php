5e9d70382651614b6100c408bb4ff435cfe31c9e
Luiz Antonio Alves da Silva <luiz.silva@grupozelo.com>
2022-07-07 01:04:23 -0300

    criado modal cpf e mascara

--- conexao.php
+++ conexao.php
@@ -2,10 +2,8 @@
 $servidor = "localhost";
 $usuario = "root";
 $senha = "";
-$dbname = "maposdev";
-//TEST-7357803479916019-101703-4e81bfa7336ac0754bd6eb94b296e531-83740263
-//APP_USR-7357803479916019-101703-8db778dcb6fae573d7a68f41298d9983-83740263
-$access_token = "TEST-7357803479916019-101703-4e81bfa7336ac0754bd6eb94b296e531-83740263";
+$dbname = "mapos";
+$access_token = "";
 //Criar a conexão
 $conexao = mysqli_connect($servidor, $usuario, $senha, $dbname);
 ?>
